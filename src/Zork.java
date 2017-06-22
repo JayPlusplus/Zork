6a918c81e509f9aa2c47df4b41a87c5180be98b0
Jay Wu <wu274319426@gmail.com>
2017-06-22 11:47:50 -0400

    zorkExtention

--- src/Zork.java
+++ src/Zork.java
@@ -3,18 +3,48 @@ import java.util.*;
 public class Zork {
 
 	public static void main(String[] args) {
-	
+		
+		Random rand = new Random();
 		boolean secret;
+		String[] items = {"n/a","Dead Scorpion","Piano","Spider","Bats","Dusty Empty Box","3 Walking Skeletons","Treasure Chest","Piles of Gold"};
+		boolean[] roomsVisited = new boolean[9];
 		int room =1;
+		int count = 0;
+		double money = 0;
 		String choice;
 		Scanner key = new Scanner(System.in);
 		System.out.println("Welcome to the house! Zorky!");
+		double[] roomMoney = new double[9];
+		for (int i=0; i<9; i++) {
+			roomMoney[i] = rand.nextDouble() * 1000;
+		}
+		int charRoom = rand.nextInt(8) + 1;
+		
+		String quit;
+		
+		
 		
 		while (true) {
+			System.out.println("You currently have " + money + " dollars");
+			count++;
+			roomsVisited[room] = true;
+			
+			if (charRoom == room) {
+				System.err.println("Monster is here, he is stealing all of your money");
+				roomMoney[charRoom] = 0;
+				money = 0;
+				charRoom = 99;
+			}
 			if (room == 1) {
+				
+				System.out.println("Money in this room is: " + roomMoney[1] );
+				money = money + roomMoney[1];
+				roomMoney[1] = 0;
+				
 				System.out.println("You are in the foyer room!");
 				System.out.println("There is a dead scorpion in the room!");	
 				System.out.println("Which direction do you want to go? (North)");
+				
 				choice = key.next();
 				if (choice.equals("North")) {
 					room = 2;
@@ -25,6 +55,11 @@ public class Zork {
 				
 			}
 			else if (room == 2) {
+				
+				System.out.println("Money in this room is: " + roomMoney[2] );
+				money = money + roomMoney[2];
+				roomMoney[2] = 0;
+				
 				System.out.println("You are in the front room!");
 				System.out.println("There is a piano in the room!");
 				System.out.println("Which direction do you want to go? (South) (West) (East)");
@@ -44,6 +79,11 @@ public class Zork {
 				
 			}
 			else if (room == 3) {
+				
+				System.out.println("Money in this room is: " + roomMoney[3] );
+				money = money + roomMoney[3];
+				roomMoney[3] = 0;
+				
 				System.out.println("You are in the libary room!");
 				System.out.println("There is a spider in the room!");
 				System.out.println("Which direction do you want to go? (North) (East)");
@@ -60,6 +100,11 @@ public class Zork {
 				
 			}
 			else if (room == 4) {
+				
+				System.out.println("Money in this room is: " + roomMoney[4] );
+				money = money + roomMoney[4];
+				roomMoney[4] = 0;
+				
 				System.out.println("You are in the kitchen room!");
 				System.out.println("There is a bats in the room!");
 				System.out.println("Which direction do you want to go? (North) (West)");
@@ -76,8 +121,13 @@ public class Zork {
 				
 			}
 			else if (room == 5) {
+				
+				System.out.println("Money in this room is: " + roomMoney[5] );
+				money = money + roomMoney[5];
+				roomMoney[5] = 0;
+				
 				System.out.println("You are in the dining room!");
-				System.out.println("There is a dust empty box in the room!");
+				System.out.println("There is a dusty empty box in the room!");
 				System.out.println("Which direction do you want to go? (South)");
 				choice = key.next();
 				if (choice.equals("South")) {
@@ -89,6 +139,11 @@ public class Zork {
 				
 			}
 			else if (room == 6) {
+				
+				System.out.println("Money in this room is: " + roomMoney[6] );
+				money = money + roomMoney[6];
+				roomMoney[6] = 0;
+				
 				System.out.println("You are in the vault room!");
 				System.out.println("There is a 3 walking skeletons in the room!");
 				System.out.println("Which direction do you want to go? (East)");
@@ -108,6 +163,10 @@ public class Zork {
 				
 			}
 			else if (room == 7) {
+				System.out.println("Money in this room is: " + roomMoney[7] );
+				money = money + roomMoney[7];
+				roomMoney[7] = 0;
+				
 				System.out.println("You are in the parlor room!");
 				System.out.println("There is a treasure chest in the room!");
 				System.out.println("Which direction do you want to go? (South) (West)");
@@ -124,6 +183,10 @@ public class Zork {
 				
 			}
 			else if (room == 8) {
+				System.out.println("Money in this room is: " + roomMoney[8] );
+				money = money + roomMoney[8];
+				roomMoney[8] = 0;
+				
 				System.out.println("You are in the secret room!");
 				System.out.println("There is a piles of gold in the room!");
 				System.out.println("Which direction do you want to go?  (West)");
@@ -134,6 +197,17 @@ public class Zork {
 				else {
 					System.out.println("Please enter a valid choice!");
 				}
+				
+			}
+			System.out.println("Would you like to quit (y/n)");
+			quit = key.next();
+			if (quit.equals("y")){
+				System.out.println("You visited " + count + " rooms and you saw:");
+				for (int i = 0; i < 9; i++){
+					if (roomsVisited[i] == true)
+						System.out.println(items[i]);
+				}
+				break;
 			}
 		}
 		
